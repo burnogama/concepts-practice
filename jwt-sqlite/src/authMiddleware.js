cc349988dcc7c989a91b7e7466c65e7adf94e276
Bruno Gama <burnogama@gmail.com>
2025-09-11 02:29:48 +0800

    practice jwt with sqlite

--- jwt-sqlite/src/authMiddleware.js
+++ jwt-sqlite/src/authMiddleware.js
@@ -2,11 +2,13 @@ import "dotenv/config";
 import jwt from "jsonwebtoken";
 
 const authMiddleware = (req, res, next) => {
-    const token = req.headers["authorization"].split(" ")[1];
+    const token = req.headers["authorization"];
 
     if (!token) return res.status(403).json({ success: false, message: "Token is required" });
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
+    const tokenWithoutBearer = token.split(" ")[1];
+
+    jwt.verify(tokenWithoutBearer, process.env.ACCESS_TOKEN_KEY, (err, decoded) => {
         if (err) return res.status(401).json({ success: false, message: "Invalid or expired token" });
 
         req.user = decoded;
